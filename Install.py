90e6b25885f3441aab5b265efae16bdbc7e79cca
jakobneufeld <jakob.n.neufeld@gmail.com>
2019-12-24 13:01:36 +0100

    Update Install.py

--- Install.py
+++ Install.py
@@ -1,1 +1,16 @@
+import os
+print("Welcome to the WINE installer")
+output = input("Choose an OS, Debian or MacOS\n")
+output.lower()
+binForMac = "https://raw.githubusercontent.com/jakobneufeld/WineInstaller/master/Winery"
+
+if output == "MacOS":
+    print("Getting Binary")
+    os.system("curl " + binForMac + " --output Wine" )
+    print("Configuring")
+    os.system("cp -f Wine /usr/local/bin/wine")
+    os.system("chmod a+x /usr/local/bin/wine")
+    print("A Sucess")
+else :
+    print("os not supported" + output)
 
